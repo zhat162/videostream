af383fdf1e96fcf11697a0f62a0a0da523ddc01a
zhat162 <leongkeezhat@outlook.com>
2024-06-29 18:21:58 +0800

    commit

--- videostream/settings.py
+++ videostream/settings.py
@@ -51,8 +51,12 @@ WSGI_APPLICATION = 'videostream.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
